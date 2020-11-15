90c09d45fdb2a0ae687b87919d28573a79ca9a2c
Sellers Crisp <sellerscrisp@Pedro.local>
2020-11-15 16:34:46 -0600

    Added form route

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
+from madlibs import stories
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "foobar"
@@ -9,7 +10,24 @@ debug = DebugToolbarExtension(app)
 
 @app.route("/")
 def index():
+    """Display stories form"""
+
     return render_template("home.html")
+
+
+@app.route("/form")
+def form():
+    """Display the required questions to complete story"""
+
+    story_choice = request.args["story_choice"]
+    story = stories[story_choice]
+
+    title = story.title
+    prompts = story.prompts
+
+    return render_template(
+        "form.html", title=title, prompts=prompts, story_choice=story_choice
+    )
 
 
 if __name__ == "__main__":
